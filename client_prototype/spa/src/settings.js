c0e065417b0cfeef120210e6a388aa31ae6d1ab7
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-03-14 14:56:02 +0300

    Run BE on localhost

--- client_prototype/spa/src/settings.js
+++ client_prototype/spa/src/settings.js
@@ -1,4 +1,4 @@
-export const LOCAL_BE_URL = 'http://55.55.55.5:6003';
+export const LOCAL_BE_URL = 'http://127.0.0.1';
 export const PASTEL_CLOUD_URL = 'http://pastel.dobrushskiy.name:2080';
 
 export const SIGN_RESOURCE_URL = LOCAL_BE_URL + '/api/sign_user_data/';
