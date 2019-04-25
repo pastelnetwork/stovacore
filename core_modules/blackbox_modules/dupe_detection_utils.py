35e92cc78816c158b9978dbdfc719cce27e90211
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-04-25 14:16:10 +0300

    Remove common code - extract to a separate repo

--- core_modules/blackbox_modules/dupe_detection_utils.py
+++ core_modules/blackbox_modules/dupe_detection_utils.py
@@ -1,70 +1,11 @@
 import multiprocessing
-import io
-from collections import OrderedDict
-
-import numpy as np
 import pandas as pd
-import keras
-from keras.applications.imagenet_utils import preprocess_input
 
 from ..settings import NetWorkSettings
 from .statistics import calculate_spearmans_rho, calculate_kendalls_tau, calculate_bootstrapped_hoeffdings
 
 
 NUM_WORKERS = int(round(multiprocessing.cpu_count() / 2))
-
-
-class _DupeDetector:
-    def __init__(self):
-        self.DUPE_DETECTION_MODELS = OrderedDict()
-
-        for modelname in NetWorkSettings.DUPE_DETECTION_MODELS:
-            if modelname == "VGG16":
-                self.DUPE_DETECTION_MODELS['VGG16'] = keras.applications.vgg16.VGG16(weights='imagenet', include_top=False, pooling='avg')
-            elif modelname == "Xception":
-                self.DUPE_DETECTION_MODELS['Xception'] = keras.applications.xception.Xception(weights='imagenet', include_top=False, pooling='avg')
-            elif modelname == "InceptionResNetV2":
-                self.DUPE_DETECTION_MODELS['InceptionResNetV2'] = keras.applications.inception_resnet_v2.InceptionResNetV2(weights='imagenet', include_top=False, pooling='avg')
-            elif modelname == "DenseNet201":
-                self.DUPE_DETECTION_MODELS['DenseNet201'] = keras.applications.DenseNet201(weights='imagenet', include_top=False, pooling='avg')
-            elif modelname == "InceptionV3":
-                self.DUPE_DETECTION_MODELS['InceptionV3'] = keras.applications.inception_v3.InceptionV3(weights='imagenet', include_top=False, pooling='avg')
-            else:
-                raise ValueError("Invalid dupe detection model name in settings: %s" % modelname)
-
-    def __prepare_fingerprint_for_export(self, image_feature_data):
-        image_feature_data_arr = np.char.mod('%f', image_feature_data)  # convert from Numpy to a list of values
-        # convert image data to float64 matrix. float64 is need for bh_sne
-        x_data = np.asarray(image_feature_data_arr).astype('float64')
-        image_fingerprint_vector = x_data.reshape((x_data.shape[0], -1))
-        return image_fingerprint_vector
-
-    def compute_deep_learning_features(self, data):
-        # read the actual image file
-        imagefile = io.BytesIO(data)
-
-        # load image
-        image = keras.preprocessing.image.load_img(imagefile, target_size=NetWorkSettings.DUPE_DETECTION_TARGET_SIZE)
-
-        # the image is now in an array of shape (3, 224, 224) but we need to expand it to (1, 2, 224, 224) as
-        # Keras is expecting a list of images
-        x = keras.preprocessing.image.img_to_array(image)
-        x = np.expand_dims(x, axis=0)
-        x = preprocess_input(x)
-
-        fingerprint_dict = {}
-        for k, v in self.DUPE_DETECTION_MODELS.items():
-            features = v.predict(x)[0]  # extract the features
-            fingerprint_vector = self.__prepare_fingerprint_for_export(features)
-            fingerprint_dict[k] = fingerprint_vector
-
-        fingerprints = []
-        for k, v in fingerprint_dict.items():
-            # REFACTOR: we inherited this interface
-            value_flattened = [x[0] for x in v.tolist()]
-            fingerprints += value_flattened
-
-        return fingerprints
 
 
 def measure_similarity(combined_fingerprint, fingerprint_table):
@@ -151,13 +92,3 @@ def assemble_fingerprints_for_pandas(db):
     final_pandas_table = pd.concat([pandas_fingerprint_table, df_vectors], axis=1,
                                    join_axes=[pandas_fingerprint_table.index])
     return final_pandas_table
-
-
-# dupe detector singleton that loads __DupeDetector lazily as the models are extremely large
-class DupeDetector:
-    _instance = None
-
-    def __new__(class_, *args, **kwargs):
-        if class_._instance is None:
-            class_._instance = _DupeDetector()
-        return class_._instance
---
+++ generate_keys.py
@@ -0,0 +1,13 @@
+import os
+from core_modules.blackbox_modules.keys import id_keypair_generation_func
+
+if __name__ == '__main__':
+    __privkey, __pubkey = id_keypair_generation_func()
+    privpath = 'private.key'
+    pubpath = 'public.key'
+    with open(privpath, "wb") as f:
+        f.write(__privkey)
+    os.chmod(privpath, 0o0700)
+    with open(pubpath, "wb") as f:
+        f.write(__pubkey)
+    os.chmod(pubpath, 0o0700)
