7ef5e2a7f68c1b69e3efd0571798de43638fbed3
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-06-26 12:23:29 +0300

    Fix `update_masternode_conf.py` script.

--- utils/update_masternode_conf.py
+++ utils/update_masternode_conf.py
@@ -5,16 +5,19 @@ import base64
 with open('/home/animecoinuser/.pastel/testnet3/masternode.conf', 'r') as f:
     config_data = f.read()
 json_config_data = json.loads(config_data)
+del json_config_data['pyAddress']
+del json_config_data['pyPubKey']
 
+mn_key = list(json_config_data.keys())[0]
 ip = urllib.request.urlopen('https://ipinfo.io/ip').read()
 ip = ip.decode('utf-8')
 ip = ip.strip()
 
-json_config_data['pyAddress'] = '{}:4444'.format(ip)
+json_config_data[mn_key]['pyAddress'] = '{}:4444'.format(ip)
 with open('/home/animecoinuser/StoVaCore/keys/public.key', 'rb') as pk_file:
     pk = pk_file.read()
 py_pub_key = base64.b64encode(pk).decode()
-json_config_data['pyPubKey'] = py_pub_key
+json_config_data[mn_key]['pyPubKey'] = py_pub_key
 final_data = json.dumps(json_config_data, indent=4, sort_keys=True)
 with open('/home/animecoinuser/.pastel/testnet3/masternode.conf', 'w') as f:
     f.write(final_data)
