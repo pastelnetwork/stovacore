fe2eac2d11718fe26c845e246c365dca8c785502
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-06-27 18:10:52 +0300

    A couple of scripts for automatic pyNode code updating (needs some improvement, but updates code on all nodes).

---
+++ for_dist/update.sh
@@ -0,0 +1,17 @@
+cd ~
+rm StoVaCore.tar.gz
+wget http://dobrushskiy.name/static/StoVaCore.tar.gz
+rm -rf keys_backup
+rm -rf model_backup
+mkdir keys_backup
+mkdir model_backup
+cp ~/StoVaCore/keys/* keys_backup/
+cp ~/StoVaCore/misc/* model_backup/
+rm -rf StoVaCore
+tar -xzf StoVaCore.tar.gz
+mkdir StoVaCore/misc
+cp keys_backup/* StoVaCore/keys/
+cp model_backup/* StoVaCore/misc/
+rm StoVaCore.tar.gz
+rm -rf keys_backup
+rm -rf model_backup
---
+++ prepare_dist.sh
@@ -0,0 +1,39 @@
+mkdir -p for_dist/StoVaCore/keys
+cp -a masternode_prototype for_dist/StoVaCore/
+cp -a core_modules for_dist/StoVaCore/
+cp -a PastelCommon for_dist/StoVaCore/
+cp -a utils for_dist/StoVaCore/
+# model is heavy - so it's copied from previous version
+# if model should be updated - uncomment the following line
+cp -a misc for_dist/StoVaCore/
+cp * for_dist/StoVaCore/
+
+rm -rf for_dist/StoVaCore/PastelCommon/__pycache__
+rm -rf for_dist/StoVaCore/PastelCommon/.git
+rm -rf for_dist/StoVaCore/PastelCommon/.gitignore
+
+rm -rf for_dist/StoVaCore/core_modules/__pycache__
+rm -rf for_dist/StoVaCore/core_modules/blackbox_modules/__pycache__
+
+rm for_dist/StoVaCore/prepare_dist.sh
+rm for_dist/StoVaCore/install_node.sh
+rm for_dist/StoVaCore/rm.spec
+rm for_dist/StoVaCore/wallet_api.spec
+
+cd for_dist
+tar -czf StoVaCore.tar.gz StoVaCore
+scp StoVaCore.tar.gz do:/var/www/static/
+scp update.sh do:/var/www/static/
+cd ..
+
+echo 'Updating StoVaCore on all masternodes..'
+HOSTS="mn2 mn3 mn4 mn5 mn6 mn7 mn8 mn9 mn10 mn11"
+#HOSTS="mn2"
+SCRIPT="source <(curl -s http://dobrushskiy.name/static/update.sh)"
+for HOSTNAME in ${HOSTS} ; do
+    ssh ${HOSTNAME} "${SCRIPT}" &
+done
+echo 'Done..'
+# finally:
+rm for_dist/StoVaCore.tar.gz
+rm -rf for_dist/StoVaCore
