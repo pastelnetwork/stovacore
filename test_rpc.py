9b5231f0c1a98e0875bb72a36167f17b6f25c3d6
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2019-09-24 10:10:47 +0300

    Mart test_rpc.py as probably unneeded

--- test_rpc.py
+++ test_rpc.py
@@ -1,21 +1,13 @@
-import os
+#FIXME: inspect, if not required - remove
+
 import asyncio
 import json
-from wallet.pastel_client import DjangoInterface
-
-APP_DIR = '/Users/alex/PycharmProjects/spa'
-KEY_PATH = 'keys'
+from wallet.pastel_client import PastelClient
 
 with open('masternodes.conf', 'r') as f:
     masternodes = json.loads(f.read())
 
-with open(os.path.join(APP_DIR, KEY_PATH, 'private.key'), "rb") as f:
-    private_key = f.read()
-
-with open(os.path.join(APP_DIR, KEY_PATH, 'public.key'), "rb") as f:
-    public_key = f.read()
-
-pastel_client = DjangoInterface(private_key, public_key, None, None, None)
+pastel_client = PastelClient(private_key, public_key, None, None, None)
 
 # mn0, mn1, mn2 = pastel_client._DjangoInterface__nodemanager.get_rpc_client_for_masternode(masternodes)
 
