f0944ff75d7cf0beecd325c4ac0d706671f2f963
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-05-25 15:47:30 +0300

    Fix create_pasteid.py util

--- utils/create_pastelid.py
+++ utils/create_pastelid.py
@@ -2,6 +2,6 @@ import os
 from cnode_connection import get_blockchain_connection
 from core_modules.blockchain import DEFAULT_PASTEL_ID_PASSPHRASE
 
-os.environ['PASTEL_ID'] = 'fakepastelid'
+os.environ.setdefault('PASTEL_ID', 'fakepastelid')
 
 get_blockchain_connection().pastelid_newkey(DEFAULT_PASTEL_ID_PASSPHRASE)
