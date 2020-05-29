8d0159e14c2c1f4a103b603a083066f8b0db9f55
Alexey Dobrushskiy <a.dobrushskiy@gmail.com>
2020-05-29 14:47:12 +0300

    Adding testnet rollout playbooks to the repository.

---
+++ ansible/generate_ssh_config_from_ip_list.py
@@ -0,0 +1,43 @@
+l = ['51.15.79.175', '51.158.176.36', '51.158.188.130', '51.15.83.99', 
+'51.158.167.70', '51.158.183.93', '51.15.109.152', '51.15.94.215', '51.15.116.190', '51.15.38.6']
+
+# Host alexmn1
+#     HostName 51.15.109.152
+#     User animecoinuser
+#     StrictHostKeyChecking no
+
+pastel_conf = '''testnet=1
+server=1
+addnode={}
+addnode={}
+addnode={}
+addnode={}
+addnode={}
+addnode={}
+addnode={}
+addnode={}
+addnode={}
+addnode={}
+gen=1
+equihashsolver=tromp
+rpcuser=rt
+rpcpassword=rt
+rpcallowip=0.0.0.0/0
+'''
+with open('mn_config', 'w') as f:
+	for i in range(0, 10):
+		f.write('Host alexmn{}\n'.format(i+1))
+		f.write('    HostName {}\n'.format(l[i]))
+		f.write('    User root\n')
+		f.write('    StrictHostKeyChecking no\n')
+		f.write('\n')
+
+	for i in range(0, 10):
+		f.write('Host _alexmn{}\n'.format(i+1))
+		f.write('    HostName {}\n'.format(l[i]))
+		f.write('    User animecoinuser\n')
+		f.write('    StrictHostKeyChecking no\n')
+		f.write('\n')
+
+with open('pastel.conf', 'w') as f:
+	f.write(pastel_conf.format(*l))
---
+++ ansible/testnet_rollout.md
@@ -0,0 +1,16 @@
+ - Create 10 instances, put IPs to `generate_ssh_config_from_ip_list.py`, include result to ssh config
+ (cp mn_config ~/.ssh/). It will also generate pastel.conf - put it to 3rd party place: 
+  - scp pastel.conf do:/var/www/static/
+
+ - execute playbook 'prepare_instance.yaml'
+ `ansible-playbook -i hosts.yaml prepare_instance.yaml -v`
+ - run cNodes (ansible ad-hoc command shell `cd ~/pastel && ./start_node.sh &`)
+ Whole command: `ansible -i hosts.yaml mns -m shell 'cd ~/pastel && ./start_node.sh &'`
+ 
+ (mine coins for some time to have enough coins for pasteleid registration)
+ - run `pynode_install.yaml` playbook
+
+ - send 1000000 coins to each MN 
+ 
+ - run `update_masternode_conf.yaml` playbook,
+ - run `convert_nodes_to_mn.yaml` playbook to convert nodes to masternodes. It will take about 5-6 hours for testnet of 10 masternodes. 
